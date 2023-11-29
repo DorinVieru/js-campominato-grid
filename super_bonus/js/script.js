1931a60cc3e695a68be337fcc8a981e42e360ce5
DorinVieru <dorino.vieru@gmail.com>
2023-11-29 17:34:55 +0100

    [UPDATE] End super bonus & improve code html & css

--- super_bonus/js/script.js
+++ super_bonus/js/script.js
@@ -1,37 +1,78 @@
 // FUNZIONE CHE CREA LA SINGOLA CASELLA DELLA GRIGLIA
-function createCell(num) {
+function createCell(num, numPerSide) {
     const element = document.createElement("div");
     element.classList.add("square");
+
+    let sideLength = `calc(100vw / ${numPerSide} - 17px)`;
+    const grid = document.getElementById("grid");
+
+    element.style.width = sideLength;
+    element.style.height = sideLength;
 
     element.innerText = num;
 
     return element;
 }
 
-// RECUPERO L'ELEMENTO CHE CONTIENE LA GRIGLIA
-const grid = document.getElementById("grid");
 
-// FUNZIONE CHE CONSENTE AL CLICK SUL BOTTONE PLAY DI FAR COMPARIRE LA GRIGLIA DI GIOCO
+// CREO FUNZIONE CHE MI GENERA LA GRIGLIA IN BASE AL LIVELLO DI DIFFICOLTA'
 function createNewGame() {
     document.getElementById("grid").innerHTML = "";
 
-    // ESEGUO UN FOR
-    for (let i = 0; i < 100; i++) {
-        // CREO LA CELLA
-        let square = createCell(i + 1);
+    let levelSelector = document.getElementById("difficulty");
+    let level = parseInt(levelSelector.value);
 
+    let cellsNumber;
+    let cellsPerSide;
+
+    // DETERMINAZIONE DELLE CASELLE PER LIVELLO
+    switch (level) {
+        case 1:
+            cellsNumber = 100
+            cellsPerSide = 10
+            break;
+        case 2:
+            cellsNumber = 81
+            cellsPerSide = 9
+            break;
+        case 3:
+            cellsNumber = 49
+            cellsPerSide = 7
+            break;
+        default:
+            cellsNumber = 100
+            cellsPerSide = 10
+    }
+
+    createPlayGround(cellsNumber, cellsPerSide)
+}
+
+
+// FUNZIONE CHE CONSENTE AL CLICK SUL BOTTONE PLAY DI FAR COMPARIRE LA GRIGLIA DI GIOCO
+function createPlayGround(totalCells, cellsPerSide){
+    
+    // RECUPERO L'ELEMENTO CHE CONTIENE LA GRIGLIA
+    const grid = document.getElementById("grid");
+
+    // ESEGUO UN FOR
+    for (let i = 0; i < totalCells; i++) {
+        // CREO LA CELLA
+        let square = createCell(i + 1, cellsPerSide);
+    
         // BONUS: AD OGNI CLICK NELLA CASELLA DELLA GRIGLIA, QUELL'ELEMENTO CAMBIA COLORORE E ME LO MOSTRA ANCHE IN CONSOLE.LOG
         square.addEventListener("click", function () {
             this.classList.toggle("clicked");
             console.log(this);
         })
-
+    
         // APPENDO LA CELLA ALLA GRIGLIA
         grid.appendChild(square);
     }
+
 }
 
-// CREO EVENTO IN ASCOLTO CHE CONSENTE DI CLICCARE IL BOTTONE E FAR PARTIRE IL GIOCO
+
+// CREO EVENTO IN ASCOLTO CHE CONSENTE DI CLICCARE IL BOTTONE E FAR PARTIRE IL GIOCO IN BASE ALLA DIFFICOLTA' SELEZIONATA
 document.getElementById("play").addEventListener("click", function () {
     createNewGame();
 })
