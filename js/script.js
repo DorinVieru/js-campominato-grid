e8c5222aa5b83315a2ab1fe1aa788e5a22f36509
DorinVieru <dorino.vieru@gmail.com>
2023-11-29 16:47:14 +0100

    [UPDATE]  Add img folder & finish html, css & js

--- js/script.js
+++ js/script.js
@@ -11,3 +11,21 @@ function createCell(num) {
 // RECUPERO L'ELEMENTO CHE CONTIENE LA GRIGLIA
 const grid = document.getElementById("grid");
 
+// FUNZIONE CHE CONSENTE AL CLICK SUL BOTTONE PLAY DI FAR COMPARIRE LA GRIGLIA DI GIOCO
+function createNewGame() {
+    document.getElementById("grid").innerHTML = "";
+
+    // ESEGUO UN FOR
+    for (let i = 0; i < 100; i++) {
+        // CREO LA CELLA
+        let square = createCell(i + 1);
+
+        // APPENDO LA CELLA ALLA GRIGLIA
+        grid.appendChild(square);
+    }
+}
+
+// CREO EVENTO IN ASCOLTO CHE CONSENTE DI CLICCARE IL BOTTONE E FAR PARTIRE IL GIOCO
+document.getElementById("play").addEventListener("click", function () {
+    createNewGame();
+})
